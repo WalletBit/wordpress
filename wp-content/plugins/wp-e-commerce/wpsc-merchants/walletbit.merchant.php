4d40d1690d26901dd00acefe098055446531b235
WalletBit <github@walletbit.com>
2013-02-03 20:40:19 +0100

    Update wp-content/plugins/wp-e-commerce/wpsc-merchants/walletbit.merchant.php
    Added support for partial payments.

--- wp-content/plugins/wp-e-commerce/wpsc-merchants/walletbit.merchant.php
+++ wp-content/plugins/wp-e-commerce/wpsc-merchants/walletbit.merchant.php
@@ -161,6 +161,11 @@
 				$sql = "UPDATE `" . WPSC_TABLE_PURCHASE_LOGS . "` SET `processed`= '3' WHERE `sessionid`=" . intval($_POST['sessionid']);
 				$wpdb->query($sql);
 			}
+			else if ($bitcoin < $purchase_log['totalprice'])
+			{
+				$sql = "UPDATE `" . WPSC_TABLE_PURCHASE_LOGS . "` SET `processed`= '2' WHERE `sessionid`=" . intval($_POST['sessionid']);
+				$wpdb->query($sql);
+			}			
 			else
 			{
 				$sql = "UPDATE `" . WPSC_TABLE_PURCHASE_LOGS . "` SET `processed`= '6' WHERE `sessionid`=" . intval($_POST['sessionid']);
